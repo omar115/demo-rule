3377a7dbb22d793ad96181081dc09202214765b8
omar_hasan <omar.hasan@udplatforms.com>
2022-12-19 13:30:28 +0600

    added

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
+
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'flask'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
